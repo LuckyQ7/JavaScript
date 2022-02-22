d96baa833c965f18896c596df24687d3e91d9de5
球球 <623903979@qq.com>
2022-02-22 20:21:29 +0800

    实现call

---
+++ 实现call/index.js
@@ -0,0 +1,25 @@
+
+
+Function.prototype._call = function _call(thisArg, ...args) {
+    // 隐式绑定this
+    const fn = this
+    // 防止是非对象类型
+    thisArg = thisArg ? Object(thisArg) : window
+    thisArg.fn = fn
+    // 传入其他参数
+    var result = thisArg.fn(...args)
+    delete thisArg.fn
+
+    return result
+}
+
+function num(n1, n2) {
+    console.log(this);
+    return n1 + n2
+}
+
+const result = num._call(2, 3, 2)
+
+const res2 = num.call(undefined, 3, 2)
+
+console.log(result, res2);
