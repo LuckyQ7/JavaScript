faf2be1bb05780510ccba2f53100ec8102f9589e
球球 <623903979@qq.com>
2022-03-08 19:51:48 +0800

    优化promise

--- 10.实现promise/index.js
+++ 10.实现promise/index.js
@@ -13,6 +13,7 @@ class MyPromise {
         const resolve = value => {
             if (this.status === PROMISE_STATUS_PENDING) {
                 queueMicrotask(() => {
+                    if (this.status !== PROMISE_STATUS_PENDING) return
                     // 状态改变放在微任务里面，否则会立即执行onfulfilled
                     this.status = PROMISE_STATUS_FULFILLED
                     this.value = value
@@ -23,60 +24,104 @@ class MyPromise {
         const reject = reason => {
             if (this.status === PROMISE_STATUS_PENDING) {
                 queueMicrotask(() => {
+                    if (this.status !== PROMISE_STATUS_PENDING) return
                     this.status = PROMISE_STATUS_REJECTED
                     this.reason = reason
                     this.onrejecteds.forEach(fn => fn(reason))
+
                 })
             }
         }
 
-        executor(resolve, reject)
+        try {
+            executor(resolve, reject)
+        } catch (error) {
+            reject(error)
+        }
+
     }
 
     then(onfulfilled, onrejected) {
-        // 如果状态已经确定 
-        if (this.status === PROMISE_STATUS_FULFILLED && onfulfilled) {
-            onfulfilled(this.value)
-        }
-        if (this.status === PROMISE_STATUS_REJECTED && onrejected) {
-            onrejected(this.reason)
-        }
+        return new MyPromise((resolve, reject) => {
+            // 如果状态已经确定 
+            if (this.status === PROMISE_STATUS_FULFILLED && onfulfilled) {
+                try {
+                    const value = onfulfilled(this.value)
+                    resolve(value)
+                } catch (error) {
+                    reject(error)
+                }
 
-        if (this.status === PROMISE_STATUS_PENDING) {
-            this.onfulfilleds.push(onfulfilled)
-            this.onrejecteds.push(onrejected)
-        }
+            }
+            if (this.status === PROMISE_STATUS_REJECTED && onrejected) {
+                try {
+                    const reason = onrejected(this.value)
+                    reject(reason)
+                } catch (error) {
+                    reject(error)
+                }
+            }
 
+            if (this.status === PROMISE_STATUS_PENDING) {
+                this.onfulfilleds.push(() => {
+                    try {
+                        const value = onfulfilled(this.value)
+                        resolve(value)
+                    } catch (error) {
+                        reject(error)
+                    }
+
+                })
+
+                this.onrejecteds.push(() => {
+                    try {
+                        const reason = onrejected(this.reason)
+                        resolve(reason)
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+
+
+            }
+        })
     }
 }
 
-
-
 const promise = new MyPromise((resolve, reject) => {
-    resolve(123)
-    // reject('123aa')
+    // resolve(123)
+    reject('123aa')
+    // throw new Error(123)
 })
 
 
 promise.then((res) => {
-    console.log(res, 'res1');
+    console.log('res1:', res,);
+    // throw new Error(123)
 }, (err) => {
-    console.log(err, 'err');
+    console.log('err1:', err);
+    return 123
+    // throw new Error(22222)
+}).then(res => {
+    console.log("res2:", res);
+}, (err) => {
+    console.log('err2:', err);
 })
 
 
 
-promise.then((res) => {
-    console.log(res, 'res2');
-}, (err) => {
-    console.log(err, 'err');
-})
+// promise.then((res) => {
+//     console.log(res, 'res2');
+// }, (err) => {
+//     console.log(err, 'err2');
+// })
 
-setTimeout(() => {
-    promise.then(res => {
-        console.log(res);
-    })
-}, 3000)
+// setTimeout(() => {
+//     promise.then(res => {
+//         console.log(res);
+//     })
+// }, 3000)
 
 // 解决多次调用
 // 解决宏任务不继续执行
+// 解决链式调用 
